abf20b837e679fa57a4ee46646bad4d5e7a769e7
Christina <christina.brady.git@gmail.com>
2015-11-04 15:56:30 -0500

    update_members.R works

--- update_members.R
+++ update_members.R
@@ -2,14 +2,9 @@
 
 update_members <- function(names, newStatus = "Inactive"){
 	source("./include/db_connection.R")
-	member_since_qry <- sprintf("SELECT member_since 
-								FROM members 
-								WHERE name IN (%s)", 
-								paste(sprintf("'%s'", names), collapse = ","))
-	member_since <- sqlQuery(tm, member_since_qry)
-	print(length(names))
-	print(length(member_since))
-	df <- data.frame(name = names, member_since = member_since$member_since, status = rep(newStatus, length(names)))
-
-	sqlUpdate(tm, df, tablename = "members", index = "status")
+	update_mem_qry <- sprintf("UPDATE members
+								SET status = '%s' 
+								WHERE name IN (%s)",
+								newStatus, paste(sprintf("'%s'", names), collapse = ","))
+	sqlQuery(tm, update_mem_qry)
 }
